23f1e073293f6a4c9feac63fd7fd37ed1f6dea53
Pierre-Baptiste <pierrebaptiste.rozic@gmail.com>
2023-05-16 17:47:38 +0200

    pba

--- models/playgrounds.js
+++ models/playgrounds.js
@@ -1,19 +1,7 @@
 const mongoose = require('mongoose');
 
-const playgroundSchema = mongoose.Schema({
-	name: String,
-    photo: String,
-    comments : [commentSchema],
-    votes: [voteSchema],
-    country:String,
-    postCode:String,
-    address: String,
-    latitude: Number,
-    longitude: Number,
-});
-
 const commentSchema = mongoose.Schema({
-    session : { type: mongoose.Schema.Types.ObjectId, ref: 'sessions' },,
+    session : { type: mongoose.Schema.Types.ObjectId, ref: 'sessions' },
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
     comment: String,
    });
@@ -23,6 +11,18 @@ const voteSchema = mongoose.Schema({
     grade: Number,
    });
 
+const playgroundSchema = mongoose.Schema({
+	name: String,
+    photo: String,
+    comments : [commentSchema],
+    votes: [voteSchema],
+    country:String,
+    postCode:String,
+    city:String,
+    address: String,
+    latitude: Number,
+    longitude: Number,
+});
 
 const Playground = mongoose.model('playgrounds', playgroundSchema);
 
