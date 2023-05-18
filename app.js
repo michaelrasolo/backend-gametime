a68cbe933696aedcda7986ebd7ac2b271b83f064
Wendy Gilles <wendygilles@hotmail.com>
2023-05-18 16:47:48 +0200

    route upload

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var playgroundsRouter = require('./routes/playgrounds');
-
+const fileUpload = require('express-fileupload');
 const cors = require("cors") // Cors installation
 
 var app = express();
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/playgrounds', playgroundsRouter);
+app.use(fileUpload());
 
 
 module.exports = app;
