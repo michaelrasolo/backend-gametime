4bbfd0a6734098b79109beb50246a16137a67e55
Julien <julien.bonyadi@gmail.com>
2023-05-18 11:03:27 +0200

    toto

--- routes/users.js
+++ routes/users.js
@@ -23,7 +23,6 @@ router.post('/signup', (req, res) => {
         nickname: req.body.nickname,
         email: req.body.email,
         password: hash,
-        nickname: req.body.nickname,
         token: uid2(32),
       });
 
